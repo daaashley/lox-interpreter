c8c853bbb464f0a22adf353ab339330e687b81c1
David Ashley <david-ashley@hotmail.com>
2023-06-15 02:46:58 +0000

    scanner and token type

--- lox/Lox.java
+++ lox/Lox.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -22,6 +24,9 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError)
+            System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -34,6 +39,7 @@ public class Lox {
             if (line == null)
                 break;
             run(line);
+            hadError = false;
         }
     }
 
