bbf7974cdb92d2e11cd435b26a2bb89f884d6782
David Ashley <david-ashley@hotmail.com>
2023-08-16 01:16:43 +0000

    finished interpreter expression pass

--- lox/Lox.java
+++ lox/Lox.java
@@ -11,7 +11,9 @@ import java.util.List;
 import lox.Scanner.*;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -30,6 +32,8 @@ public class Lox {
 
         if (hadError)
             System.exit(65);
+        if (hadRuntimeError)
+            System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -57,11 +61,17 @@ public class Lox {
         if (hadError)
             return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
