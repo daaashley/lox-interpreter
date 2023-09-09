7603df90d79ca56a0256c3fbe7aaf31b3fa8dc8a
David Ashley <david-ashley@hotmail.com>
2023-09-09 18:29:55 +0000

    semi

--- lox/Parser.java
+++ lox/Parser.java
@@ -273,7 +273,7 @@ public class Parser {
         List<Expr> arguments = new ArrayList<>();
         if (!check(RIGHT_PAREN)) {
             do {
-                if (arguments.size() >= 255) { 
+                if (arguments.size() >= 255) {
                     error(peek(), "Can't have more than 255 arguments.");
                 }
                 arguments.add(expression());
@@ -281,7 +281,7 @@ public class Parser {
         }
         Token paren = consume(RIGHT_PAREN, "Expect ')' after arguments.");
 
-        return new Expr.Call(callee, paren,arguments)
+        return new Expr.Call(callee, paren, arguments);
     }
 
     private Expr call() {
