1579e8be6ce8e996d5217c5af17f853d69b8cb9f
David Ashley <david-ashley@hotmail.com>
2023-09-09 18:13:06 +0000

    java clock native function

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -5,7 +5,28 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     // Will be reassigned based on block scoping
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    // Lox is 'lisp-1', vars and func stored in same namespace
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
